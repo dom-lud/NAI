bbae718b2492f90d6a9ad8a44efe1b754704e2f7
Bartosz Dembowski <116431313+s29602-pj@users.noreply.github.com>
2025-11-06 17:17:02 +0100

    Dodaje program

---
+++ Lab_2/fuzzyFan.py
@@ -0,0 +1,62 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+
+
+# --- Zmienne wejściowe ---
+temperature = ctrl.Antecedent(np.arange(0, 41, 1), 'temperature')
+humidity = ctrl.Antecedent(np.arange(0, 101, 1), 'humidity')
+air_quality = ctrl.Antecedent(np.arange(0, 501, 1), 'air_quality')
+
+
+# --- Zmienna wyjściowa ---
+fan_speed = ctrl.Consequent(np.arange(0, 101, 1), 'fan_speed')
+
+
+# --- Funkcje przynależności ---
+temperature['low'] = fuzz.trimf(temperature.universe, [0, 0, 20])
+temperature['medium'] = fuzz.trimf(temperature.universe, [10, 20, 30])
+temperature['high'] = fuzz.trimf(temperature.universe, [20, 40, 40])
+
+
+humidity['low'] = fuzz.trimf(humidity.universe, [0, 0, 50])
+humidity['medium'] = fuzz.trimf(humidity.universe, [30, 50, 70])
+humidity['high'] = fuzz.trimf(humidity.universe, [50, 100, 100])
+
+
+air_quality['good'] = fuzz.trimf(air_quality.universe, [0, 0, 100])
+air_quality['medium'] = fuzz.trimf(air_quality.universe, [50, 150, 250])
+air_quality['bad'] = fuzz.trimf(air_quality.universe, [200, 500, 500])
+
+
+fan_speed['low'] = fuzz.trimf(fan_speed.universe, [0, 0, 40])
+fan_speed['medium'] = fuzz.trimf(fan_speed.universe, [30, 50, 70])
+fan_speed['high'] = fuzz.trimf(fan_speed.universe, [60, 100, 100])
+
+
+# --- Reguły rozmyte ---
+r1 = ctrl.Rule((temperature['high'] & humidity['high']) | (air_quality['bad']), fan_speed['high'])
+r2 = ctrl.Rule((temperature['medium'] & humidity['medium']) | (air_quality['medium']), fan_speed['medium'])
+r3 = ctrl.Rule((temperature['low'] & humidity['low'] & air_quality['good']), fan_speed['low'])
+r4 = ctrl.Rule((temperature['high'] & air_quality['medium']), fan_speed['high'])
+r5 = ctrl.Rule((humidity['high'] & air_quality['medium']), fan_speed['medium'])
+r6 = ctrl.Rule((temperature['low'] & humidity['high']), fan_speed['medium'])
+
+
+# --- System ---
+fan_ctrl = ctrl.ControlSystem([r1, r2, r3, r4, r5, r6])
+fan_sim = ctrl.ControlSystemSimulation(fan_ctrl)
+
+
+# --- Przykład ---
+fan_sim.input['temperature'] = 28
+fan_sim.input['humidity'] = 65
+fan_sim.input['air_quality'] = 180
+fan_sim.compute()
+print(f"Prędkość wentylatora: {fan_sim.output['fan_speed']:.2f}%")
+
+
+# --- Wykresy przynależności ---
+temperature.view(); humidity.view(); air_quality.view(); fan_speed.view();
+plt.show()
